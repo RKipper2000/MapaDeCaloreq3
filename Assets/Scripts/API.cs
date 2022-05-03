05001c820a56de2f409b73b5595203d7e5f7983a
CracklyDuck <trejo.fabian@hotmail.com>
2022-05-03 12:02:16 -0500

    API 2.0

--- Assets/Scripts/API.cs
+++ Assets/Scripts/API.cs
@@ -6,10 +6,37 @@ using UnityEngine.Networking;
 
 public class API : MonoBehaviour
 {
-    string results;
+
+    [System.Serializable]
+
+    public class Lectura
+    {
+
+        public double valor;
+        public double latitud;
+        public double longitud;
+    }
+
+    [System.Serializable]
+    public class lecturaslist
+    {
+        public Lectura[] lectura;
+    }
+
+    public lecturaslist mylecturaslist = new lecturaslist();
+
+
+
+    public string results;
+
+
+
     public IEnumerator GetData()
     {
-        UnityWebRequest www = UnityWebRequest.Get("http://localhost:3000/api/getLogs");
+        string sensor = "1";
+        string sensorreq = "localhost:3000/api/getDataBySector/"+sensor;
+        Debug.Log(sensorreq);
+        UnityWebRequest www = UnityWebRequest.Get(sensorreq);
         yield return www.Send();
 
         if (www.isNetworkError)
@@ -18,23 +45,36 @@ public class API : MonoBehaviour
         }
         else
         {
+            //Debug.Log("Success");
+
             // Show results as text
-            Debug.Log(www.downloadHandler.text);
+
 
             // Or retrieve results as binary data
             results = www.downloadHandler.text;
+
+            mylecturaslist = JsonUtility.FromJson<lecturaslist>(("{\"lectura\":" + www.downloadHandler.text + "}"));
         }
     }
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(GetData());
+
+        
+
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(results);
+        //Debug.Log(results);
         
+
+
     }
 }
